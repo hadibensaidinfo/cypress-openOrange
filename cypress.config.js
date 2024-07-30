bb1a56205a1758aa7c0b29d3d1602b80f479e740
Hedi.bensaid <hadibensaid@gmail.com>
2024-07-30 20:02:03 +0200

    FEAT: Ajouter le plugin "cypress-mochawesome-reporter"

--- cypress.config.js
+++ cypress.config.js
@@ -5,23 +5,20 @@ const fs = require('fs')
 
 module.exports = defineConfig({
   // setupNodeEvents can be defined in either
-  // the e2e or component configuration
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Report-OpenOrange',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    saveJson: true
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      on('after:spec', (spec, results) => {
-        if (results && results.video) {
-          // Do we have failures for any retry attempts?
-          const failures = results.tests.some((test) =>
-            test.attempts.some((attempt) => attempt.state === 'failed')
-          )
-          if (!failures) {
-            // delete the video if the spec passed and no tests retried
-            fs.unlinkSync(results.video)
-          }
-        }
-      })
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
-    baseUrl:'https://opensource-demo.orangehrmlive.com/'
+    baseUrl: 'https://opensource-demo.orangehrmlive.com/'
 
   },
 })
